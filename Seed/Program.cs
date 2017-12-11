6b63dcbeecf95792fbdee266ea0318956dc82216
GFHOME\bsouza <bsouza@gamefly.com>
2017-12-11 10:33:11 -0800

    App Settings

--- Seed/Program.cs
+++ Seed/Program.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Serilog;
 using Serilog.Events;
 
 namespace Seed {
 	public class Program {
 
+		/// <summary>
+		/// The Main entry point into the application
+		/// </summary>
+		/// <param name="args">The arguments.</param>
 		public static void Main(string[] args) {
 
 			// Configure logging before anything so that we can ensure that
@@ -17,13 +24,20 @@ namespace Seed {
 			BuildWebHost(args).Run();
 		}
 
+		/// <summary>
+		/// Builds the web host.
+		/// </summary>
+		/// <param name="args">The arguments.</param>
+		/// <returns></returns>
 		public static IWebHost BuildWebHost(string[] args) {
 			return WebHost.CreateDefaultBuilder(args)
 				.CaptureStartupErrors(true)
-				.UseStartup<Startup>()
-				.UseSerilog()
+				.ConfigureAppConfiguration((hostContext, config) => AddConfig(config, args))
+				.UseStartup<Startup>().UseSerilog()
 				.Build();
 		}
+
+		#region Private Methods
 
 		/// <summary>
 		/// Creates the logger for the application
@@ -36,11 +50,10 @@ namespace Seed {
 				.MinimumLevel.Override("System", LogEventLevel.Information)
 				.Enrich.FromLogContext()
 				.WriteTo.Async(c => c.File(
-						"../Logs/appLog_.txt",
-						outputTemplate: GetLoggingTemplate(),
-						rollingInterval: RollingInterval.Day
-					)
-				);
+					"../Logs/appLog_.txt",
+					outputTemplate: GetLoggingTemplate(),
+					rollingInterval: RollingInterval.Day
+				));
 
 #if DEBUG
 			//we will only use the console logger when debugging
@@ -55,6 +68,32 @@ namespace Seed {
 		private static string GetLoggingTemplate() {
 			return "[{Timestamp:HH:mm:ss} {Level:u3}] [{RequestId}] [{SourceContext}] {Message:lj}{NewLine}{Exception}";
 		}
+
+		/// <summary>
+		/// Adds the configuration for the specified environment.
+		/// </summary>
+		/// <param name="configBuilder">The configuration builder.</param>
+		/// <param name="args">The arguments.</param>
+		/// <returns>the config builder</returns>
+		private static IConfigurationBuilder AddConfig(IConfigurationBuilder configBuilder, string[] args = null) {
+
+			string environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "development";
+
+			Log.Information("Loading configuration for environment: {environment}", environmentName);
+
+			configBuilder.Sources.Clear();
+			configBuilder.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("host/appsettings.json", false, true)
+				.AddJsonFile($"host/environment/appsettings.{environmentName}.json", true, true);
+
+			if (args != null) {
+				configBuilder.AddCommandLine(args);
+			}
+
+			return configBuilder;
+		}
+
+		#endregion
 
 	}
 }
