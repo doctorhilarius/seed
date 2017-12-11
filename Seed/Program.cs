f237d97bb6e2a0899affa12d830b80291dd9db70
GFHOME\bsouza <bsouza@gamefly.com>
2017-12-11 09:46:30 -0800

    3. Install and Configure Logging

--- Seed/Program.cs
+++ Seed/Program.cs
@@ -1,25 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
-namespace Seed
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+namespace Seed {
+	public class Program {
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
+		public static void Main(string[] args) {
+
+			// Configure logging before anything so that we can ensure that
+			// application configuration and startup errors are logged
+			// unfortunately the logger requires configuration so we will take the 
+			// hit of loading the config twice but at this time the overhead is minimal
+			Log.Logger = CreateLogger();
+
+			BuildWebHost(args).Run();
+		}
+
+		public static IWebHost BuildWebHost(string[] args) {
+			return WebHost.CreateDefaultBuilder(args)
+				.CaptureStartupErrors(true)
+				.UseStartup<Startup>()
+				.UseSerilog()
+				.Build();
+		}
+
+		/// <summary>
+		/// Creates the logger for the application
+		/// </summary>
+		private static ILogger CreateLogger() {
+
+			var loggerConfig = new LoggerConfiguration()
+				.MinimumLevel.Debug()
+				.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+				.MinimumLevel.Override("System", LogEventLevel.Information)
+				.Enrich.FromLogContext()
+				.WriteTo.Async(c => c.File(
+						"../Logs/appLog_.txt",
+						outputTemplate: GetLoggingTemplate(),
+						rollingInterval: RollingInterval.Day
+					)
+				);
+
+#if DEBUG
+			//we will only use the console logger when debugging
+			loggerConfig.WriteTo.Console(outputTemplate: GetLoggingTemplate());
+#endif
+			return loggerConfig.CreateLogger();
+		}
+
+		/// <summary>
+		/// Gets the logging template to use for log messages
+		/// </summary>
+		private static string GetLoggingTemplate() {
+			return "[{Timestamp:HH:mm:ss} {Level:u3}] [{RequestId}] [{SourceContext}] {Message:lj}{NewLine}{Exception}";
+		}
+
+	}
 }
