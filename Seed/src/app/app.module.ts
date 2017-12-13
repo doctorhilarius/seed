f576a8b6743c9530ec7d05c90109fba4c12e8481
GFHOME\bsouza <bsouza@gamefly.com>
2017-12-13 10:58:21 -0800

    21. Install and Configure Service Worker

--- Seed/src/app/app.module.ts
+++ Seed/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js'),
   ],
   providers: [],
   bootstrap: [AppComponent]
