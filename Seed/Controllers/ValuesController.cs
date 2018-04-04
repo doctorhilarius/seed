114a65c8218ab7b841e1c78aafb5d791a21dbe19
Brooke Souza <brooke.souza@rouseservices.com>
2018-04-04 09:41:38 -0700

    update packages switch to sass

--- Seed/Controllers/ValuesController.cs
+++ Seed/Controllers/ValuesController.cs
@@ -1,7 +1,4 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Seed.Controllers
@@ -13,7 +10,7 @@ namespace Seed.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new[] { "value1", "value2" };
         }
 
         // GET api/values/5
